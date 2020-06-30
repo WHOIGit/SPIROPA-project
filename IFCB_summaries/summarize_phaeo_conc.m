f884802f51643fc37b49a1501117d1bf4c0a0f7d
hsosik <hsosik@whoi.edu>
2020-06-30 17:39:29 -0400

    save cast results for phaeocystis colonies

--- IFCB_summaries/summarize_phaeo_conc.m
+++ IFCB_summaries/summarize_phaeo_conc.m
@@ -33,3 +33,27 @@ save(['\\sosiknas1\IFCB_products\SPIROPA\summary\phaeo_colony_' cruisestr], 'met
 
 
 
+load('\\sosiknas1\IFCB_products\SPIROPA\summary\summary_biovol_allHDF_min20_2018.mat')
+cruisestr = 'AR29';
+cc = strmatch('Phaeo', class2use);
+uwii = find(strcmp(meta_data.sample_type, 'underway') & strcmp(meta_data.cruise, cruisestr) & ~meta_data.skip);
+castii = find(strcmp(meta_data.sample_type, 'cast') & strcmp(meta_data.cruise, cruisestr) & ~meta_data.skip);
+meta_data = meta_data(castii,:);
+Phaeocystis_colony_CNN_predicted_concentration_per_ml = classcount(castii,cc)./meta_data.ml_analyzed;
+mdate = mdate(castii);
+save(['\\sosiknas1\IFCB_products\SPIROPA\summary\phaeo_colony_cast_samples_' cruisestr], 'meta_data', 'Phaeocystis_colony_CNN_predicted_concentration_per_ml', 'classpath_generic', 'mdate', 'cruisestr')
+
+clear
+
+
+load('\\sosiknas1\IFCB_products\NESLTER_transect\summary\summary_biovol_allHDF_min20_2018.mat');
+cruisestr = 'AR28B';
+cc = strmatch('Phaeo', class2use);
+uwii = find(strcmp(meta_data.sample_type, 'underway') & strcmp(meta_data.cruise, cruisestr) & ~meta_data.skip);
+castii = find(strcmp(meta_data.sample_type, 'cast') & strcmp(meta_data.cruise, cruisestr) & ~meta_data.skip);
+meta_data = meta_data(castii,:);
+Phaeocystis_colony_CNN_predicted_concentration_per_ml = classcount(castii,cc)./meta_data.ml_analyzed;
+mdate = mdate(castii);
+save(['\\sosiknas1\IFCB_products\SPIROPA\summary\phaeo_colony_cast_samples_' cruisestr], 'meta_data', 'Phaeocystis_colony_CNN_predicted_concentration_per_ml', 'classpath_generic', 'mdate', 'cruisestr')
+
+clear
